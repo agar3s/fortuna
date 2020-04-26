9f7671c46c037d3d717a49fcf32c571394982f24
Agar3s <ekeisco@gmail.com>
2020-04-26 14:16:33 -0500

    simple reroll cubes

--- Cubes/CubeSet.gd
+++ Cubes/CubeSet.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 export (Array, int) var cube_indexes = [0, 1, 2]
 
+var roll_count = 0
+var roll_limit = 3
 
 func _ready():
 	var i = 0
@@ -10,6 +12,16 @@ func _ready():
 		i += 1
 
 func roll_cubes():
+	if not can_roll(): return
+	
 	for cube in $Cubes.get_children():
 		cube.roll()
 	
+	roll_count += 1
+
+func can_roll():
+	return roll_count < roll_limit
+
+func reset():
+	roll_count = 0
+
--- Sandbox/SubsetCubes.gd
+++ Sandbox/SubsetCubes.gd
@@ -1,8 +1,25 @@
 extends Node2D
 
+var cube_debug_text = "Faces: %s %s %s\nRoll Count: %s\nCan roll?: %s"
 
 func _ready():
-	$Button.connect("button_down", self, 'roll_cubes')
+	$Roll.connect("button_down", self, 'roll_cubes')
+	$Launch.connect("button_down", self, 'reset_cubes')
 
 func roll_cubes():
 	$CubeSet.roll_cubes()
+	update_debug_text()
+
+func reset_cubes():
+	$CubeSet.reset()
+	update_debug_text()
+
+
+func update_debug_text():
+	$DebugSet1.text = cube_debug_text % [
+		$CubeSet/Cubes/Cube1.face_up,
+		$CubeSet/Cubes/Cube2.face_up,
+		$CubeSet/Cubes/Cube3.face_up,
+		$CubeSet.roll_count,
+		$CubeSet.can_roll()
+	]
